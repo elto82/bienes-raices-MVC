1dbed887834caf7cf4686f1e3bc3861fc7ca72e5
elto82 <elto.82@gmail.com>
2024-06-22 19:17:51 -0500

    variables de entorno

--- config/db.js
+++ config/db.js
@@ -1,18 +1,25 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 
-const db = new Sequelize("bienesraices_node_mvc", "root", "", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  define: {
-    timestamps: true,
-  },
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+const db = new Sequelize(
+  process.env.DB_NOMBRE,
+  process.env.BD_USER,
+  process.env.BD_PASS ?? "",
+  {
+    host: process.env.BD_HOST,
+    port: 3306,
+    dialect: "mysql",
+    define: {
+      timestamps: true,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
 export default db;
