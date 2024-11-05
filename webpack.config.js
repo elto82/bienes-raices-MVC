e0b85b3cb0eb9f955be97ae962e32bb1559fdaf1
elto82 <elto.82@gmail.com>
2024-11-04 19:11:42 -0500

    dropzone

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 import path from "path";
+import { agregarImagen } from "./controllers/propiedadCortroller.js";
 
 export default {
   mode: "development",
   entry: {
     mapa: "./src/js/mapa.js",
+    agregarImagen: "./src/js/agregarImagen.js",
   },
   output: {
     filename: "[name].js",
