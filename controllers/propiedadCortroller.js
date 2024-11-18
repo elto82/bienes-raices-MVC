67ce613dcdba11f7b60d4df2e87fdd5c86fa4592
elto82 <elto.82@gmail.com>
2024-11-18 18:37:32 -0500

    pagina de propiedad publica

--- controllers/propiedadCortroller.js
+++ controllers/propiedadCortroller.js
@@ -1,5 +1,6 @@
 import { validationResult } from "express-validator";
 import { Precio, Categoria, Propiedad } from "../models/index.js";
+import { unlink } from "node:fs/promises";
 
 const admin = async (req, res) => {
   const { id } = req.usuario;
@@ -7,8 +8,18 @@ const admin = async (req, res) => {
     where: {
       usuarioId: id,
     },
+    include: [
+      {
+        model: Categoria,
+        as: "categoria",
+      },
+      {
+        model: Precio,
+        as: "precio",
+      },
+    ],
   });
-  console.log(propiedades);
+  // console.log(propiedades);
 
   res.render("propiedades/admin", {
     pagina: "Mis propiedades",
@@ -147,4 +158,149 @@ const almacenarImagen = async (req, res, next) => {
   }
 };
 
-export { admin, crear, guardar, agregarImagen, almacenarImagen };
+const editar = async (req, res) => {
+  const { id } = req.params;
+  //validar que la propiedad exista
+  const propiedad = await Propiedad.findByPk(id);
+  if (!propiedad) {
+    return res.redirect("/misPropiedades");
+  }
+  //validar que la propiedad pertenece a quien visita esta pagina
+  if (req.usuario.id.toString() !== propiedad.usuarioId.toString()) {
+    return res.redirect("/misPropiedades");
+  }
+  //consultar modelo precio y categoria
+  const [precios, categorias] = await Promise.all([
+    Precio.findAll(),
+    Categoria.findAll(),
+  ]);
+
+  res.render("propiedades/editar", {
+    pagina: `Editar Propiedad: ${propiedad.titulo}`,
+    csrfToken: req.csrfToken(),
+    categorias,
+    precios,
+    datos: propiedad,
+  });
+};
+
+const guardarCambios = async (req, res) => {
+  //verificar la validacion
+  let resultado = validationResult(req);
+
+  if (!resultado.isEmpty()) {
+    //consultar modelo precio y categoria
+    const [precios, categorias] = await Promise.all([
+      Precio.findAll(),
+      Categoria.findAll(),
+    ]);
+    res.render("propiedades/editar", {
+      pagina: "Editar Propiedades",
+      csrfToken: req.csrfToken(),
+      categorias,
+      precios,
+      errores: resultado.array(),
+      datos: req.body,
+    });
+  }
+  const { id } = req.params;
+  //validar que la propiedad exista
+  const propiedad = await Propiedad.findByPk(id);
+  if (!propiedad) {
+    return res.redirect("/misPropiedades");
+  }
+  //validar que la propiedad pertenece a quien visita esta pagina
+  if (req.usuario.id.toString() !== propiedad.usuarioId.toString()) {
+    return res.redirect("/misPropiedades");
+  }
+
+  //reescribir propiedad y actualizarla
+  try {
+    const {
+      titulo,
+      descripcion,
+      habitaciones,
+      estacionamiento,
+      wc,
+      calle,
+      lat,
+      lng,
+      precio: precioId,
+      categoria: categoriaId,
+    } = req.body;
+
+    propiedad.set({
+      titulo,
+      descripcion,
+      habitaciones,
+      estacionamiento,
+      wc,
+      calle,
+      lat,
+      lng,
+      precioId,
+      categoriaId,
+    });
+    await propiedad.save();
+    res.redirect("/misPropiedades");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const eliminar = async (req, res) => {
+  const { id } = req.params;
+  //validar que la propiedad exista
+  const propiedad = await Propiedad.findByPk(id);
+  if (!propiedad) {
+    return res.redirect("/misPropiedades");
+  }
+  //validar que la propiedad pertenece a quien visita esta pagina
+  if (propiedad.usuarioId.toString() !== req.usuario.id.toString()) {
+    return res.redirect("/misPropiedades");
+  }
+  //eliminar la imagen asociada
+  await unlink(`public/uploads/${propiedad.imagen}`);
+
+  //eliminar la propiedad
+  await propiedad.destroy();
+  res.redirect("/misPropiedades");
+};
+
+//Muestra una propiedad
+const mostrarPropiedad = async (req, res) => {
+  const { id } = req.params;
+  //validar que la propiedad exista
+  const propiedad = await Propiedad.findByPk(id, {
+    include: [
+      {
+        model: Categoria,
+        as: "categoria",
+      },
+      {
+        model: Precio,
+        as: "precio",
+      },
+    ],
+  });
+  if (!propiedad || !propiedad.publicado) {
+    return res.redirect("/404");
+  }
+
+  res.render("propiedades/mostrar", {
+    pagina: propiedad.titulo,
+    propiedad,
+  });
+};
+
+export {
+  admin,
+  crear,
+  guardar,
+  agregarImagen,
+  almacenarImagen,
+  editar,
+  guardarCambios,
+  eliminar,
+  mostrarPropiedad,
+};
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -6,6 +6,10 @@ import {
   crear,
   guardar,
   almacenarImagen,
+  editar,
+  guardarCambios,
+  eliminar,
+  mostrarPropiedad,
 } from "../controllers/propiedadCortroller.js";
 import protegerRuta from "../middleware/protegerRuta.js";
 import upload from "../middleware/subirImagen.js";
@@ -13,7 +17,9 @@ import upload from "../middleware/subirImagen.js";
 const router = Router();
 
 router.get("/misPropiedades", protegerRuta, admin);
+
 router.get("/propiedades/crear", protegerRuta, crear);
+
 router.post(
   "/propiedades/crear",
   protegerRuta,
@@ -46,5 +52,35 @@ router.post(
   upload.single("imagen"),
   almacenarImagen
 );
+
+router.get("/propiedades/editar/:id", protegerRuta, editar);
+
+router.post(
+  "/propiedades/editar/:id",
+  protegerRuta,
+  body("titulo").notEmpty().withMessage("El titulo es obligatorio"),
+  body("descripcion")
+    .notEmpty()
+    .withMessage("La descripción es obligatoria")
+    .isLength({ min: 50 })
+    .withMessage("La descripción debe tener al menos 50 caracteres"),
+  body("categoria").isNumeric().withMessage("Selecciona una categoría"),
+  body("precio").isNumeric().withMessage("Selecciona un rango de precios"),
+  body("habitaciones")
+    .isNumeric()
+    .withMessage("Selecciona la cantidad de habitaciones"),
+  body("estacionamiento")
+    .isNumeric()
+    .withMessage("Selecciona la cantidad de estacionamientos"),
+  body("wc").isNumeric().withMessage("Selecciona la cantidad de baños"),
+  body("lat").notEmpty().withMessage("Ubica la propiedad en el mapa"),
+  body("lng").notEmpty().withMessage("Ubica la propiedad en el mapa"),
+  guardarCambios
+);
+
+router.post("/propiedades/eliminar/:id", protegerRuta, eliminar);
+
+//Area publica
+router.get("/propiedad/:id", mostrarPropiedad);
 
 export default router;
