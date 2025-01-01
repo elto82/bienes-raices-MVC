9f011c9c5e097fe6fad01e39f03b1ba65b07719e
elto82 <elto.82@gmail.com>
2025-01-01 12:11:24 -0500

    mostrar en pantalla de inicio propiedades

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,16 +1,44 @@
 import { Precio, Categoria, Propiedad } from "../models/index.js";
 
 const inicio = async (req, res) => {
-  const [precios, categorias] = await Promise.all([
+  const [precios, categorias, casas, departamentos] = await Promise.all([
     Precio.findAll({ raw: true }),
     Categoria.findAll({ raw: true }),
+    Propiedad.findAll({
+      limit: 3,
+      where: {
+        categoriaId: 1,
+      },
+      include: [
+        {
+          model: Precio,
+          as: "precio",
+        },
+      ],
+      order: [["createdAt", "DESC"]],
+    }),
+    Propiedad.findAll({
+      limit: 3,
+      where: {
+        categoriaId: 2,
+      },
+      include: [
+        {
+          model: Precio,
+          as: "precio",
+        },
+      ],
+      order: [["createdAt", "DESC"]],
+    }),
   ]);
-  console.log(categorias);
+  // console.log(categorias);
 
   res.render("inicio", {
     pagina: "Inicio",
     categorias,
     precios,
+    casas,
+    departamentos,
   });
 };
 const categoria = (req, res) => {};
